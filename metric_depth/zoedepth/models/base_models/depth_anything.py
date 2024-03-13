8c3df0ec883587ff87257aba637cc7488d37f79e
xbillowy <xbillowy@gmail.com>
2024-03-13 11:34:02 +0800

    fix: fix np.int64 type error of F.interpolate()

--- metric_depth/zoedepth/models/base_models/depth_anything.py
+++ metric_depth/zoedepth/models/base_models/depth_anything.py
@@ -169,9 +169,14 @@ class Resize(object):
 
         return (new_width, new_height)
 
+    # def __call__(self, x):
+    #     width, height = self.get_size(*x.shape[-2:][::-1])
+    #     return nn.functional.interpolate(x, (height, width), mode='bilinear', align_corners=True)
+
+    # TODO: modified, convert the input size type to `int` to avoid error
     def __call__(self, x):
         width, height = self.get_size(*x.shape[-2:][::-1])
-        return nn.functional.interpolate(x, (height, width), mode='bilinear', align_corners=True)
+        return nn.functional.interpolate(x, (int(height), int(width)), mode='bilinear', align_corners=True)
 
 class PrepForMidas(object):
     def __init__(self, resize_mode="minimal", keep_aspect_ratio=True, img_size=384, do_resize=True):
